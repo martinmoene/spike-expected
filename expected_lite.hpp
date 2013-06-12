20b16d6e10d81a12c5ec2176028f2cbaaf01890f
Martin Moene <martin.moene@gmail.com>
2013-06-12 13:46:20 +0200

    Add initial implementation

--- expected_lite.hpp
+++ expected_lite.hpp
@@ -11,76 +11,256 @@
 // authored by Fernando Cacciola and Andrzej Krzemienski and the
 // Boost.Expected proposal authored by Pierre Talbot.
 
-#ifndef NONSTD_EXPECTED_HPP
-#define NONSTD_EXPECTED_HPP
+#ifndef NONSTD_EXPECTED_LITE_HPP
+#define NONSTD_EXPECTED_LITE_HPP
 
-#include <exception>
+#include <new>          // placement new
+#include <stdexcept>    // std:logic_error
+#include <utility>      // std::swap()
 
 #if __cplusplus < 201103L       // non-C++11 compilers
 # include "exception_c++11.h"   // std::exception_ptr, std::move()
 #endif
 
-namespace nonstd
-{
+namespace nonstd {
 
-const struct nullexp_t {} nullexp;
-
-const struct only_set_initialized_t{} only_set_initialized{};
-
+#define NONSTD_EXPECTED_LITE_MAX( a, b ) ( (a) > (b) ? a : b )
 
 template <typename T, typename E>
-struct expected_base
+class expected;
+
+/**
+ * exception on error access when no error is contained.
+ */
+class bad_expected_access : public std::logic_error
 {
+public:
+  explicit bad_expected_access( const char * what_arg )
+  : logic_error( what_arg ) {}
+};
+
+/**
+ * C++03 constructed union to hold value or error.
+ */
+template <typename T, typename E>
+union storage_t
+{
+    friend class expected<T,E>;
+
+private:
     typedef T value_type;
     typedef E error_type;
 
-#if __cplusplus >= 201103L
-    union
+    void construct_value( value_type const & v )
     {
-        value_type m_value;
-        error_type m_error;
-    };
-#else
-    value_type m_value;
-    error_type m_error;
-#endif
-    bool m_valid;
+        new( value_ptr() ) value_type( v );
+    }
 
-    expected_base( value_type const & rhs )
-    : m_value( rhs ), m_valid( true ) {}
+    void destruct_value()
+    {
+        // Note: VC6 requires the use of the template parameter T
+        value_ptr()->~T();
+    }
 
-    expected_base( nullexp_t, error_type err )
-    : m_error( err ), m_valid( false ) {}
+    void construct_error( error_type const & e )
+    {
+        new( error_ptr() ) error_type( e );
+    }
 
-    expected_base( only_set_initialized, bool b )
-    : m_valid( b ) {}
+    void destruct_error()
+    {
+        // Note: VC6 requires the use of the template parameter T
+        error_ptr()->~E();
+    }
+
+    value_type const & value() const
+    {
+        return * value_ptr();
+    }
+
+    value_type & value()
+    {
+        return * value_ptr();
+    }
+
+    error_type const & error() const
+    {
+        return * error_ptr();
+    }
+
+    error_type & error()
+    {
+        return * error_ptr();
+    }
+
+    value_type * value_ptr() const
+    {
+        return as( (value_type*)0 );
+    }
+
+    error_type * error_ptr() const
+    {
+        return as( (error_type *)0 );
+    }
+
+    // Note: VC6 cannot handle as<T>():
+
+    template <typename U>
+    U * as( U* ) const
+    {
+        return reinterpret_cast<U*>( const_cast<unsigned char *>( buffer ) );
+    }
+
+    unsigned char dummy;
+    unsigned char buffer[ NONSTD_EXPECTED_LITE_MAX( sizeof(T), sizeof(E) ) ];
 };
 
+#undef NONSTD_EXPECTED_LITE_MAX
+
+const struct nullexp_t {} nullexp;
+// const struct exceptional_t {} exceptional;
+
+/**
+ * expected value or error.
+ */
 template <typename T, typename E = std::exception_ptr>
-class expected : private expected_base<T,E>
+class expected
 {
 public:
     typedef T value_type;
     typedef E error_type;
 
+    // constructors
+
     expected( value_type const & rhs )
-    : expected_base<T,E>( rhs ) {}
-
-    expected( nullexp_t, error_type const & err )
-    : expected_base<T,E>( nullexp, err ) {}
-
-    expected( expected const & rhs )
-    : expected_base<T,E>( only_set_initialized, rhs.m_valid )
+    : has_value( true )
     {
-        if ( rhs.initialized() ) new ( value_ptr() ) T( *rhs );
-        else                     new ( error_ptr() ) E( rhs.error )
+        storage.construct_value( rhs );
     }
 
-    ~expected() {}
+    expected( nullexp_t, error_type const & rhs )
+    : has_value( false )
+    {
+        storage.construct_error( rhs );
+    }
+
+    expected( expected const & rhs )
+    : has_value( rhs.has_value )
+    {
+        if ( has_value ) storage.construct_value( rhs.storage.value() );
+        else             storage.construct_error( rhs.storage.error() );
+    }
+
+    // destructor
+
+    ~expected()
+    {
+        if ( has_value ) storage.destruct_value();
+        else             storage.destruct_error();
+    }
+
+    // assignment
+
+#ifdef NONSTD_EXPECTED_DEFINE_ASSIGNMENT
+
+    expected & operator =( expected rhs )
+    {
+        rhs.swap( *this );
+        return *this;
+    }
+#else
+    private: expected & operator =( expected rhs );
+    public:
+#endif // NONSTD_EXPECTED_DEFINE_ASSIGNMENT
+
+    // swap
+
+    void swap( expected & rhs )
+    {
+        using std::swap;
+
+        if ( has_value )
+        {
+            if ( rhs.has_value )
+            {
+                swap( value(), rhs.value() );
+            }
+            else
+            {
+                error_type t = rhs.storage.error();
+
+                rhs.storage.construct_value( value() );
+                storage.construct_error( t );
+
+                swap( has_value, rhs.has_value );
+            }
+        }
+        else
+        {
+            if ( rhs.has_value )
+            {
+                rhs.swap( *this );
+            }
+            else
+            {
+                swap( storage.error(), rhs.storage.error() );
+            }
+        }
+    }
+
+    // observers
+
+    value_type const * operator ->() const
+    {
+        return has_value ? storage.value_ptr() : ( std::rethrow_exception( storage.error() ), storage.value_ptr() );
+    }
+
+    value_type * operator ->()
+    {
+        return has_value ? storage.value_ptr() : ( std::rethrow_exception( storage.error() ), storage.value_ptr() );
+    }
+
+    value_type const & operator *() const
+    {
+        return has_value ? * storage.value_ptr() : ( std::rethrow_exception( storage.error() ), * storage.value_ptr() );
+    }
+
+    value_type & operator *()
+    {
+        return has_value ? * storage.value_ptr() : ( std::rethrow_exception( storage.error() ), * storage.value_ptr() );
+    }
+
+    /*explicit*/ operator bool() const
+    {
+        return has_value;
+    }
+
+    value_type const & value() const
+    {
+        return **this;
+    }
+
+    value_type & value()
+    {
+        return **this;
+    }
+
+    template <typename V>
+    value_type value_or( V const & v ) const
+    {
+        return *this ? **this : v;
+    }
+
+    error_type const & error() const
+    {
+        return has_value ? (throw nonstd::bad_expected_access("expected: no contained error"), storage.error() ) : storage.error();
+    }
 
 private:
+    bool has_value;
+    storage_t<T,E> storage;
 };
 
 } // namespace nonstd
 
-#endif // NONSTD_EXPECTED_HPP
+#endif // NONSTD_EXPECTED_LITE_HPP
--- test_expected_lite.cpp
+++ test_expected_lite.cpp
@@ -1,11 +1,85 @@
+// Copyright (C) 2013 Martin Moene.
+//
+// Implementation note: this version targets C++03.
+//
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// The idea is based on the Expected<T> class template authored by
+// Andrei Alexandrescu. The interface is based on the std::optional proposal
+// authored by Fernando Cacciola and Andrzej Krzemienski and the
+// Boost.Expected proposal authored by Pierre Talbot.
 
+#define NONSTD_EXPECTED_DEFINE_ASSIGNMENT
 
-#include "expected-lite.hpp"
+#include "expected_lite.hpp"
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+struct S
+{
+    S(std::string n) : n(n)     { std::cout << n << ": S()" << std::endl; }
+    S(S const & r) : n(r.n+"@") { std::cout << n << ": S(S const &)" << std::endl; }
+   ~S()                         { std::cout << n << ": ~S()" << std::endl; }
+
+   const std::string n;
+};
 
 int main()
 {
-    nonstd::expected<int> ei = 33;
+    using namespace nonstd;
+
+    try
+    {
+        expected<int> ei = 33;
+        std::cout << "ei-33: " << *ei << std::endl;
+
+        int a = ei.value();
+        int & b = ei.value();
+        int & c = *ei;
+        std::cout << "ei.value_or(22): " << ei.value_or(22) << std::endl;
+
+        b = 77;
+        std::cout << "ei-77: " << *ei << std::endl;
+        std::cout << "c    : " << *ei << std::endl;
+
+        expected<int> ek( 99 );
+        std::cout << "ek   : " << *ek << std::endl;
+#ifdef NONSTD_EXPECTED_DEFINE_ASSIGNMENT
+        ek = ei;
+        std::cout << "ek=ei: " << *(ek = ei) << std::endl;
+#endif
+//        std::exception_ptr p = ei.error();
+
+        expected< S > es1 ( S("es1") );
+        expected< S > es2 = S("es2");
+
+        expected<int> ee1 ( nullexp, std::make_exception_ptr( std::runtime_error("ee1") ) );
+
+        assert( ! !!ee1 );
+        std::cout << "ee1.value_or(22): " << ee1.value_or(22) << std::endl;
+
+        int & x = *ee1;
+    }
+    catch ( std::runtime_error const & e )
+    {
+        std::cout << "std::runtime_error: " << e.what() << std::endl;
+    }
+    catch ( std::exception const & e )
+    {
+        std::cout << "std::exception: " << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        assert( false );
+    }
+
+    return 0; // VC6
 }
 
-// cl -nologo -W3 -EHsc -Ideps test-expected-lite.cpp && test-expected-lite
-// g++ -Wall -Wextra -std=c++11 -o test-expected-lite.exe test-expected-lite.cpp && test-expected-lite
+// cl -nologo -W3 -EHsc -Ideps test_expected_lite.cpp && test_expected_lite
+// g++ -Wall -Wextra -std=c++03 -Ideps -o test_expected_lite.exe test_expected_lite.cpp && test_expected_lite
+// g++ -Wall -Wextra -std=c++11 -o test_expected_lite.exe test_expected_lite.cpp && test_expected_lite
