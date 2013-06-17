aa37773dfde259a4adf80f262e85050121ae079e
Martin Moene <martin.moene@gmail.com>
2013-06-17 11:47:25 +0200

    Rename example1

--- examples/example_disengaged.cpp
+++ examples/example_disengaged.cpp
@@ -61,6 +61,6 @@ int main()
     return 0; // VC6
 }
 
-// cl -nologo -W3 -EHsc -.. -I../deps example1.cpp && example1
-// g++ -Wall -Wextra -std=c++03 -I.. -I../deps -o example1.exe example1.cpp && example1
-// g++ -Wall -Wextra -std=c++11 -I.. -o example1.exe example1.cpp && example1
+// cl -nologo -W3 -EHsc -I.. -I../deps example_disengaged.cpp && example_disengaged
+// g++ -Wall -Wextra -std=c++03 -I.. -I../deps -o example_disengaged.exe example_disengaged.cpp && example_disengaged
+// g++ -Wall -Wextra -std=c++11 -I.. -o example_disengaged.exe example_disengaged.cpp && example_disengaged
