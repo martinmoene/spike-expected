da9c9dfeaf25b0d20bc3da96646aec62aa484a83
Martin Moene <martin.moene@gmail.com>
2013-06-17 12:56:02 +0200

    Rename header to .hpp

--- nonco/test_expected.cpp
+++ nonco/test_expected.cpp
@@ -4,7 +4,7 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include "expected.h"
+#include "expected.hpp"
 
 #include <cassert>
 #include <iostream>
